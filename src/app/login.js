d27854a577a8034efbf0fccba92c9a608f3630cb
adelinegueniat <gueniat.adeline@gmail.com>
2016-12-21 14:45:59 +0100

    Revert "Revert "Ajout des fonctions loggedAsUser/Admin""
    This reverts commit b523233dd037d5b76ca1fadfb1fdeeb381c231f2.

--- src/app/login.js
+++ src/app/login.js
@@ -1,7 +1,22 @@
 function loginController() {
-  this.text = 'My brand new component!';
-}
+  var $ctrl = this;
+  $ctrl.text = '';
+  $ctrl.loggedAs = 'déconnecté'
 
+  $ctrl.loggedAsAdmin = function () {
+    $ctrl.isAdmin = true;
+    $ctrl.isUser = false;
+    $ctrl.loggedAs = 'administrateur';
+  };
+
+  $ctrl.loggedAsUser = function () {
+    $ctrl.isUser = true;
+    $ctrl.isAdmin = false;
+    $ctrl.loggedAs = 'utilisateur';
+  };
+
+
+}
 angular
   .module('app')
   .component('login', {
