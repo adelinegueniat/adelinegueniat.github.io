3d424b480d16644c4895124436ca3ec8d67ccf2d
adelinegueniat <gueniat.adeline@gmail.com>
2016-12-21 14:31:04 +0100

    Modification d'une variable

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,9 +1,10 @@
+
 angular
   .module('app')
-  .component('accueil', {
+  .component('hello', {
     templateUrl: 'app/hello.html',
     controller: function () {
       var $ctrl = this; // Ajout pour sécuriser le this
-      $ctrl.accueil = 'Bienvenue';
+      $ctrl.hello = 'Bienvenue';
     }
   });
