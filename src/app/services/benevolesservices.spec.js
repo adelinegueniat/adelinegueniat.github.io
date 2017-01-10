1e257ceddc1c77b708d6eb34d654b7c62ea76d52
adelinegueniat <gueniat.adeline@gmail.com>
2017-01-10 14:06:15 +0100

    suppression du services benevoles, tout est géré dans benevoles.js

--- src/app/services/benevolesservices.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Benevolesservices service', function () {
-  beforeEach(module('app'));
-  it('should', angular.mock.inject(function (Benevolesservices) {
-    expect(Benevolesservices.getData()).toEqual(3);
-  }));
-});
