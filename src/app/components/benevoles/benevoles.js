1a04e1e470b8ec69d1fe41eaa92c4c12392b698e
adelinegueniat <gueniat.adeline@gmail.com>
2017-01-11 09:15:56 +0100

    Tri des bénévoles selon leur état, ajout d'une fonction de suppression

--- src/app/components/benevoles/benevoles.js
+++ src/app/components/benevoles/benevoles.js
@@ -8,7 +8,7 @@ function benevolesController() {
   $ctrl.address = '';
   $ctrl.phone = '';
   var listebenevoleskey = 'lbkey';
-saveLocalStorage();
+
   // saveLocalStorage() si nouvelle key //
 
   $ctrl.benevoles = getListe();
@@ -29,10 +29,34 @@ saveLocalStorage();
     var firstName = $ctrl.firstName;
     var address = $ctrl.address;
     var phone = $ctrl.phone;
-    var benevole = [id, titre, name, firstName, address, phone];
+    var onDuty = false;
+    var benevole = [id, titre, name, firstName, address, phone, onDuty];
     $ctrl.benevoles.push(benevole);
     saveLocalStorage();
   };
+
+  $ctrl.getBenevolePosition = function () {
+    var position = $ctrl.benevoles.indexOf(); // on utilise la méthode indexOf() pour obtenir la position du nom entré
+    return position;
+  };
+
+  $ctrl.getBenevole = function () {
+    var position = $ctrl.getBenevolePosition();
+    var benevole = $ctrl.benevoles[position];
+    return benevole;
+  };
+
+  $ctrl.removeBenevole = function ($index) {
+    $ctrl.benevoles.splice($index, 1);
+    localStorage.removeItem($index);
+    saveLocalStorage();
+  };
+
+  $ctrl.toggle = function (onDuty) {
+    return !onDuty;
+  };
+
+  $ctrl.putOffDuty = function () { };
 }
 
 angular
